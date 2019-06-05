144ab52843809578eae73f9df365155f0d426b15
beginner-frontender <44082329+damon5378@users.noreply.github.com>
2019-06-05 16:56:29 +0300

    Add files via upload

--- lesson-19/src/js/main.js
+++ lesson-19/src/js/main.js
@@ -16,13 +16,13 @@
   			prevArrow: $('.arrows__left'),
   			nextArrow: $('.arrows__right'),
   			responsive: [
-  				{
-  					breakpoint: 1200,
-  					settings: {
-  						slidesToShow: 2,
-  						slidesToScroll: 1,
-  					}
-  				},
+  				// {
+  				// 	breakpoint: 1200,
+  				// 	settings: {
+  				// 		slidesToShow: 3,
+  				// 		slidesToScroll: 1,
+  				// 	}
+  				// },
   				{
   					breakpoint: 992,
   					settings: {
