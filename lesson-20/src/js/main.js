01de49578670ab95faa843736aa6488175f90e36
beginner-frontender <44082329+damon5378@users.noreply.github.com>
2019-06-05 16:59:26 +0300

    Add files via upload

---
+++ lesson-20/src/js/main.js
@@ -0,0 +1,58 @@
+// var button = document.querySelector('#button');
+// var modal = document.querySelector('#modal');
+// var close = document.querySelector('#close');
+
+// button.addEventListener('click', function() {
+// 	modal.classList.add('modal_active');
+// });
+
+// close.addEventListener('click', function() {
+// 	modal.classList.remove('modal_active')
+// });
+new WOW().init();
+  	$(document).ready(function(){
+  		$('.slider').slick({
+  			slidesToShow: 3,
+  			slidesToScroll: 1,
+  			prevArrow: $('.arrows__left'),
+  			nextArrow: $('.arrows__right'),
+  			responsive: [
+  				{
+  					breakpoint: 1200,
+  					settings: {
+  						slidesToShow: 2,
+  						slidesToScroll: 1,
+  					}
+  				},
+  				{
+  					breakpoint: 992,
+  					settings: {
+  						slidesToShow: 2,
+  						slidesToScroll: 1
+  					}
+  				},
+  				{
+  					breakpoint: 768,
+  					settings: {
+  						slidesToShow: 1,
+  						slidesToScroll: 1
+  					}
+  				},
+  				{
+  					breakpoint: 576,
+  					settings: {
+  						slidesToShow: 1,
+  						slidesToScroll: 1
+  					}
+  				},
+  				{
+  					breakpoint: 320,
+  					settings: {
+  						slidesToShow: 1,
+  						slidesToScroll: 1
+  					}
+  				}
+  			]
+  		});
+  	});
+
